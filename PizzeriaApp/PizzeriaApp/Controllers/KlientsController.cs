49dfd986f706a199913fa86926256485941e9dd5
Maciej Karnicki <s17129@pjwstk.edu.pl>
2019-12-07 16:05:08 +0100

    added more controllers

---
+++ PizzeriaApp/PizzeriaApp/Controllers/KlientsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzeriaApp.Models;
+
+namespace PizzeriaApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KlientsController : ControllerBase
+    {
+        private s17129Context _context;
+        public KlientsController(s17129Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetKlients()
+        {
+            return Ok(_context.Klient.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetKlient(int id)
+        {
+            var klient = _context.Klient.FirstOrDefault(e => e.IdUser == id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+            return Ok(klient);
+        }
+    }
+}
---
+++ PizzeriaApp/PizzeriaApp/Controllers/ZamowieniesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzeriaApp.Models;
+
+namespace PizzeriaApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZamowieniesController : ControllerBase
+    {
+        private s17129Context _context;
+        public ZamowieniesController(s17129Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetZamowienies()
+        {
+            return Ok(_context.Zamowienie.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetZamowienie(int id)
+        {
+            var zamowienie = _context.Zamowienie.FirstOrDefault(e => e.IdZamowienie == id);
+            if (zamowienie == null)
+            {
+                return NotFound();
+            }
+            return Ok(zamowienie);
+        }
+    }
+}
